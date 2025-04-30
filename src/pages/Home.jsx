500e70720cb44f09b0cb04be5c567127c199dd14
ShafiqUr-Rehman <shafiqurrehmanbscs2022@gmail.com>
2025-04-30 23:28:45 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {  
       try {
-        const res = await fetch('https://real-estate-mern-backend-rho.vercel.app/api/listing/get?offer=true&limit=4');
+        const res = await fetch('api/listing/get?offer=true&limit=4');
         const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
@@ -40,7 +40,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch('https://real-estate-mern-backend-rho.vercel.app/api/listing/get?type=rent&limit=4');
+        const res = await fetch('api/listing/get?type=rent&limit=4');
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
@@ -51,7 +51,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('https://real-estate-mern-backend-rho.vercel.app/api/listing/get?type=sale&limit=4');
+        const res = await fetch('api/listing/get?type=sale&limit=4');
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
